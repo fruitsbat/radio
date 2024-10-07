ca0f58c1d18eb8941a98a5d426f9c3c26f055e52
zoe <zoe@kittycat.homes>
2024-10-07 20:14:33 +0200

    basic parsing

--- components/NowPlaying.vue
+++ components/NowPlaying.vue
@@ -1,13 +1,14 @@
 <template>
-  <div v-if="data">{{ data }} {{ status }}</div>
+  <div v-if="pub">{{ pub }} {{ status }}</div>
 </template>
 
 <script setup lang="ts">
 import type { AzuraApiReturn } from "~/scripts/nowplaying";
 import { BASE_URL } from "~/scripts/variables";
-const { status, data, send } = useWebSocket<
-  { pub: { data: { np: any } } }
->(`wss://${BASE_URL}/api/live/nowplaying/websocket`, { autoReconnect: true });
+const { status, data, send } = useWebSocket<string>(
+  `wss://${BASE_URL}/api/live/nowplaying/websocket`,
+  { autoReconnect: true }
+);
 send(
   JSON.stringify({
     subs: {
@@ -15,4 +16,16 @@ send(
     },
   })
 );
+const pub = computed((): string | undefined => {
+  try {
+    const parsed = JSON.parse(data.value ?? "");
+    if (parsed.pub.data.np) {
+      return parsed.pub.data.np;
+    }
+  } catch {
+    return undefined;
+  }
+
+  return undefined;
+});
 </script>
