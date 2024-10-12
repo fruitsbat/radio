019dffa40d8dc506412fc9232930167da6846820
bat <zoe@kittycat.homes>
2024-10-12 21:14:26 +0200

    e

--- components/PlaybackControls.vue
+++ components/PlaybackControls.vue
@@ -3,7 +3,7 @@
   <div class="bg-base-100 bg-opacity-100">
     <div class="flex items-center gap-4 p-4">
       <PlayPauseButton />
-      <PlaybackControlTrackInfo class="flex-grow" />
+      <PlaybackControlTrackInfo />
       <VolumeSlider />
     </div>
   </div>
