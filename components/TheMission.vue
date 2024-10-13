a94d1419ced4679633b10c287370e48758116d8c
bat <zoe@kittycat.homes>
2024-10-13 20:41:25 +0200

    e

--- components/TheMission.vue
+++ components/TheMission.vue
@@ -1,10 +1,16 @@
 <template>
   <template>
     <div class="flex w-full flex-col items-center">
-      <h2 class="font-black px-8 text-center text-7xl" id="artists">
-        <a href="#artists">the mission</a>
+      <h2 class="font-black px-8 text-center text-7xl" id="the-mission">
+        <a href="#the-mission">the mission</a>
       </h2>
-      <div class="grid"></div>
+      <p class="px-8 text-justify text-lg max-w-4xl">
+        this project is supposed to offer an easy way to discover new music and
+        have your music heard by others. algorithms on many music platforms will
+        almost never recommend smaller artists so they're hard to find. i want
+        kittycat.homes radio to be an alternative to this. it should be as easy
+        as possible to find new music.
+      </p>
     </div>
   </template>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,13 @@
 <template>
-  <div class="bg-base-100 h-[100dvh] break-words flex flex-col items-stretch text-neutral">
+  <div
+    class="bg-base-100 h-[100dvh] break-words flex flex-col items-stretch text-neutral"
+  >
     <div class="flex-1 gap-24 pb-24 flex flex-col overflow-y-scroll">
       <ClientOnly class="h-full">
         <NowPlaying />
       </ClientOnly>
       <ArtistsList />
+      <Submit />
       <TheMission />
     </div>
     <ClientOnly>
