89f58c2135c2bb42abbeef5d64b49323268a3177
bat <zoe@kittycat.homes>
2024-10-13 20:57:55 +0200

    dsf

--- components/SubmitYourMusic.vue
+++ components/SubmitYourMusic.vue
@@ -1,29 +1,27 @@
 <template>
-  <template>
-    <div class="flex w-full flex-col items-center">
-      <h2 class="font-black px-8 text-center text-7xl" id="submit">
-        <a href="#submit">submit</a>
-      </h2>
-      <p class="px-8 text-justify text-lg max-w-4xl">
-        if you want to be featured (if yes thank you you're awesome btw), send
-        an email to
-        <a class="link" href="mailto://zoe@kittycat.homes">zoe@kittycat.homes</a
-        >. or send me a dm on discord. i'm @fruits_bat!
-        <br />
-        <br />
-        this station doesn't make any money and i'm not planning on running ads
-        or anything like that anytime soon (ads suck! just let everyone listen to
-        music!), so sadly i can't offer any royalties. if this does change
-        through some unlikely turn of events and it goes past being just self
-        sustaining i'll be sure to let everyone know.
-        <br />
-        <br />
-        you <strong>will</strong> get the satisfaction of having contributed to
-        a community project and maybe some new ears on your music.
-        <br />
-        <br />
-        i'm excited to hear from everyone! :)
-      </p>
-    </div>
-  </template>
+  <div class="w-full flex flex-col items-center">
+    <h2 class="font-black px-8 text-center text-7xl" id="submit">
+      <a href="#submit">submit</a>
+    </h2>
+    <p class="w-full px-8 text-justify text-lg max-w-4xl">
+      if you want to be featured (if yes thank you you're awesome btw), send an
+      email to
+      <a class="link" href="mailto://zoe@kittycat.homes">zoe@kittycat.homes</a>.
+      or send me a dm on discord. i'm @fruits_bat!
+      <br />
+      <br />
+      this station doesn't make any money and i'm not planning on running ads or
+      anything like that anytime soon (ads suck! just let everyone listen to
+      music!), so sadly i can't offer any royalties. if this does change through
+      some unlikely turn of events and it goes past being just self sustaining
+      i'll be sure to let everyone know.
+      <br />
+      <br />
+      you <strong>will</strong> get the satisfaction of having contributed to a
+      community project and maybe some new ears on your music.
+      <br />
+      <br />
+      i'm excited to hear from everyone!
+    </p>
+  </div>
 </template>
--- components/TheMission.vue
+++ components/TheMission.vue
@@ -1,20 +1,17 @@
 <template>
-  <template>
-    <div class="flex w-full flex-col items-center">
-      <h2 class="font-black px-8 text-center text-7xl" id="the-mission">
-        <a href="#the-mission">the mission</a>
-      </h2>
-      <p class="px-8 text-justify text-lg max-w-4xl">
-        this project is supposed to offer an easy way to discover new music and
-        have your music heard by others. algorithms on many music platforms will
-        almost never recommend smaller artists so they're hard to find. i want
-        kittycat.homes radio to be an alternative to this. it should be as easy
-        as possible to find new music.
-        <br />
-        <br />
-        i also want this radio station to provide a pleasant listening
-        experience.
-      </p>
-    </div>
-  </template>
+  <div class="flex w-full flex-col items-center">
+    <h2 class="font-black px-8 text-center text-7xl" id="the-mission">
+      <a href="#the-mission">the mission</a>
+    </h2>
+    <p class="w-full px-8 text-justify text-lg max-w-4xl">
+      this project is supposed to offer an easy way to discover new music and
+      have your music heard by others. algorithms on many music platforms will
+      almost never recommend smaller artists so they're hard to find. i want
+      kittycat.homes radio to be an alternative to this. it should be as easy as
+      possible to find new music.
+      <br />
+      <br />
+      i also want this radio station to provide a pleasant listening experience.
+    </p>
+  </div>
 </template>
