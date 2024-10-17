48f3d42c3275a9de6556f96bcb760b7fe2e6438e
bat <zoe@kittycat.homes>
2024-10-17 21:42:52 +0200

    e

--- components/PlaybackControlTrackInfo.vue
+++ components/PlaybackControlTrackInfo.vue
@@ -12,7 +12,7 @@
     </a>
     <a
       title="show this artist some love!"
-      :href="`/#${artistLink}`"
+      :href="getArtistIdFromName(np?.now_playing.song.artist ?? '')"
       class="link flex items-center justify-center gap-2"
     >
       <UserIcon class="h-6 w-6" />
@@ -23,13 +23,9 @@
 
 <script setup lang="ts">
 import { useNowPlayingStore } from "~/scripts/nowplaying";
-import { useAudioPlayerStore } from "~/scripts/audioPlayer";
+import { getArtistIdFromName } from "~/scripts/artists";
 import { MusicalNoteIcon, UserIcon } from "@heroicons/vue/16/solid";
-const mediaControlsStore = useAudioPlayerStore();
 const npStore = useNowPlayingStore();
 const np = computed(() => npStore.lastValidResponse);
 const loading = computed(() => npStore.lastValidResponse === undefined);
-const artistLink = computed(() =>
-  np.value?.now_playing.song.artist.replaceAll(" ", "-")
-);
 </script>
