cbdc02beadba035181b19b93858231a3d5c12968
bat <zoe@kittycat.homes>
2024-10-18 19:44:26 +0200

    add link

---
+++ components/KittycatHomesFooter.vue
@@ -0,0 +1,16 @@
+<template>
+  <NuxtLink
+    to="https://kittycat.homes"
+    :external="true"
+    class="flex flex-col items-center link"
+  >
+    <small class="w-full text-center">
+      kittycats rule teh world forevah © 2024
+    </small>
+    <img
+      class="w-[176px] h-[62px]"
+      src="/button.png"
+      style="image-rendering: pixelated"
+    />
+  </NuxtLink>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -9,16 +9,7 @@
       <ArtistsList />
       <SubmitYourMusic />
       <TheMission />
-      <div class="flex flex-col items-center">
-        <small class="w-full text-center">
-          kittycats rule teh world forevah © 2024
-        </small>
-        <img
-          class="w-[176px] h-[62px]"
-          src="/button.png"
-          style="image-rendering: pixelated"
-        />
-      </div>
+      <KittycatHomesFooter />
     </div>
     <ClientOnly>
       <PlaybackControls />
